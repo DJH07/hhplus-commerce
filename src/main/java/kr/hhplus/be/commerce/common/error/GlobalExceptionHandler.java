3e25238b0f1c7fc406f5d75e8f6e669c5fb13dc3
DJH07 <hdj00868@gmail.com>
2025-01-17 04:51:44 +0900

    feat: API 호출 및 에러 로깅 기능 추가 (요청 URI, 본문, 응답 본문, 에러 메시지 로그)

--- src/main/java/kr/hhplus/be/commerce/common/error/GlobalExceptionHandler.java
+++ src/main/java/kr/hhplus/be/commerce/common/error/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.commerce.common.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,10 +11,13 @@ import org.springframework.web.method.annotation.HandlerMethodValidationExceptio
 import java.util.List;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("MethodArgumentNotValidException: Invalid request content.", ex);
+
         List<ErrorResponse> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
@@ -30,11 +34,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HandlerMethodValidationException.class)
     public ResponseEntity<ErrorResponse> handleHandlerMethodValidationException(HandlerMethodValidationException ex) {
+        log.error("HandlerMethodValidationException: {}", ex.getMessage(), ex);
+
         return buildErrorResponse(HttpStatus.BAD_REQUEST, "400", "유효성 검사 실패: " + ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        log.error("Exception: Unknown error occurred.", ex);
+
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "500", "알 수 없는 오류가 발생했습니다: " + ex.getMessage());
     }
 
